8bb1f623208a9b561fd9aca58ed898c817ad066d
Aniket Saha <aniketsahaworkspace@gmail.com>
2025-01-04 14:55:58 +0530

    commit

--- snippets/backend_js/server.js
+++ snippets/backend_js/server.js
@@ -1,1 +1,13 @@
-console.log("hello world")
+const fs= require("fs")
+
+fs.writeFile("./snippets/backend_js/first.txt", "Writing my first file using node", ()=>{
+    console.log("done writing");
+    fs.readFile("./snippets/backend_js/first.txt",(e,d)=>{
+        console.log(e,d.toString());
+        console.log("Done reading");
+    })
+})
+
+fs.appendFile("./snippets/backend_js/first.txt", "Appending information",()=>{
+    console.log("Done Appending");
+})
